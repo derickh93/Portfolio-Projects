1329e23ffdde49cd9e937a5ee27fc25a853ea37a
Derick Hansraj <derickhansraj@ymail.com>
2021-08-06 17:18:32 -0400

    updated twilio and crashing

--- Review/StripePayment/server/index.js
+++ Review/StripePayment/server/index.js
@@ -7,20 +7,51 @@ const cors = require("cors");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(cors());
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
+////////////////////////////////////////////////////////
+app.post("/twilio-message", cors(), async (req, res) => {
+  try {
+    let { Message, SendTo } = req.body;
+
+    const conf = await client.messages
+      .create({
+        body: Message,
+        from: "+15176842560",
+        to: SendTo,
+      })
+      .then((message) => console.log(message.sid));
+    res.json({
+      message: "text sent",
+      success: true,
+      result: conf,
+    });
+  } catch (error) {
+    console.log("Error", error);
+    res.json({
+      message: "text not sent",
+      success: false,
+    });
+  }
+});
+////////////////////////////////////////////////////////
+
 app.post("/payment", cors(), async (req, res) => {
-  let { amount, id, cid } = req.body;
+  let { amount, id, cid, email } = req.body;
   try {
     const payment = await stripe.paymentIntents.create({
       amount,
       currency: "USD",
-      description: "Spatula company",
+      description: "Minimum charge for laundry pickup and delivery",
       payment_method: id,
       confirm: true,
       setup_future_usage: "off_session",
       customer: cid,
+      receipt_email: email,
     });
     console.log("Payment", payment);
     res.json({
@@ -66,6 +97,7 @@ app.post("/create", cors(), async (req, res) => {
 app.post("/create-customer-portal-session", cors(), async (req, res) => {
   try {
     let { cid } = req.body;
+    console.log(cid);
     const session = await stripe.billingPortal.sessions.create({
       customer: cid,
       return_url: "https://localhost:3000",
@@ -93,16 +125,22 @@ app.post("/create-customer-portal-session", cors(), async (req, res) => {
 
 app.post("/add-address", cors(), async (req, res) => {
   try {
-    let { cid, address, city, state, full_name } = req.body;
+    let { cid, address, city, state, address2, full_name, options } = req.body;
+    const state_zip = state.split(" ");
+    console.log(state_zip);
+    console.log(options);
     const customer = await stripe.customers.update(cid, {
+      metadata: options,
+
       shipping: {
         name: full_name,
         address: {
           line1: address,
+          line2: address2,
           city: city,
-          state: state,
+          state: state_zip[1],
           country: "US",
-          postal_code: "94111",
+          postal_code: state_zip[2],
         },
       },
     });
@@ -172,6 +210,42 @@ app.post("/get-cards", cors(), async (req, res) => {
   }
 });
 ///////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////
+async function invoiceItem(custID) {
+  await stripe.invoiceItems.create({
+    customer: custID,
+    price: "price_1JJoJOEkFqXnuEeNweXUvsf4",
+  });
+}
+//////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////
+app.post("/create-invoice", cors(), async (req, res) => {
+  try {
+    let { custID, md } = req.body;
+    console.log(custID);
+    invoiceItem(custID);
+
+    const invoice = await stripe.invoices.create({
+      customer: custID,
+      metadata: md,
+    });
+
+    res.json({
+      message: "cards retrieved",
+      success: true,
+      result: invoice,
+    });
+  } catch (error) {
+    console.log("Error", error);
+    res.json({
+      message: "cards not retrieved",
+      success: false,
+    });
+  }
+});
+/////////////////////////////////////////////////
 app.listen(process.env.PORT || 4000, () => {
   console.log("Sever is listening on port 4000");
 });
